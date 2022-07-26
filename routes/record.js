8bd4d6b6068e058da5034d7095400d24d4872191
ivan.montes.contractor <ivan.montes.contractor@bbva.com>
2022-07-26 18:36:55 -0500

    no se xD

--- routes/record.js
+++ routes/record.js
@@ -5,12 +5,10 @@ const ObjectId = require("mongodb").ObjectId;
 
 recordRoutes.route("/login").get(function (req, res) {
   let db_connect = dbo.getDb("cyclingMain");
-	console.log(db_connect);
 	db_connect
 		.collection("employees")
 		.find({})
 		.toArray(function (err, result) {
-      if (err) throw err;
       res.json(result);
     });
 })
